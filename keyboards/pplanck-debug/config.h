c940cbad06bbf93dd830acac2175ee7d2cfb1e43
Alexander Black <liekkronoa@gmail.com>
2016-07-31 03:04:57 -0400

    Initial Commit

--- keyboards/pplanck-debug/config.h
+++ keyboards/pplanck-debug/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 14
 
 /* Planck PCB default pin-out */
-#define MATRIX_COL_PINS {B6, B2, B3, B1, F7, F6, F5, F4, D3, D2, D1, D0, D4, C6 }
+#define MATRIX_COL_PINS {C6, D4, D0, D1, D2, D3, F4, F5, F6, F7, B1, B3, B2, B6}
 #define MATRIX_ROW_PINS {D7, E6, B4, B5 }
 
 /* COL2ROW or ROW2COL */
--- keyboards/pplanck-debug/keymaps/default/keymap.c
+++ keyboards/pplanck-debug/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LBRC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T, /* */  KC_ESC,  KC_BSPC,  /* */  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RBRC},
     {KC_QUOT, KC_A,  KC_S,  KC_D,  KC_F,  KC_G, /* */  KC_TAB,  KC_ENTER, /* */  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS },
     {KC_MINS, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, /* */  KC_LSFT, KC_RGUI,  /* */  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL},
-    {MO(1),   KC_NO, KC_LCTL, KC_LALT, KC_LGUI, LT(3, KC_9),    KC_SPC,    LT(2, KC_0),   KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
+    {MO(1),   KC_NO, KC_LCTL, KC_LALT, KC_LGUI, LT(3, KC_9),  KC_SPC, KC_SPC,    LT(2, KC_0),   KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
   },
 
   /* Data ENTRY
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LBRC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T, /* */  KC_ESC,  KC_BSPC,  /* */   KC_LABK,  KC_PAST,  KC_PPLS, KC_7,  KC_8, KC_9},
     {KC_QUOT, KC_A,  KC_S,  KC_D,  KC_F,  KC_G, /* */  KC_TAB,  KC_ENTER, /* */   KC_RABK,  KC_PSLS,  KC_PMNS, KC_4,  KC_5, KC_6},
     {KC_MINS, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, /* */  KC_LSFT, KC_RGUI,  /* */   KC_DOT,   KC_PEQL,  KC_0,    KC_1,  KC_2, KC_3},
-    {KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, MO(3),  /* */  KC_SPC,  /* */  MO(2),  KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
+    {KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, MO(3),  /* */  KC_SPC, KC_SPC, /* */  MO(2),  KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
   },
 
   // TODO: Map raised keys to code
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_F1,   KC_F2,   KC_F3,   KC_F4,   S(KC_9), S(KC_0),    /* */ KC_ESC,  KC_DELETE,   /* */ S(KC_6),  S(KC_7), S(KC_8), KC_7,  KC_8, KC_9},
     {KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_QUOT, S(KC_QUOT), /* */ KC_TAB, KC_GRV,      /* */  S(KC_3),  S(KC_4),  S(KC_5), KC_4,  KC_5, KC_6},
     {KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MINS, S(KC_MINS), /* */  KC_LSFT,  KC_ENTER, /* */  S(KC_1),  S(KC_2), KC_0,    KC_1,  KC_2, KC_3},
-    {KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_TRNS,    /* */  KC_SPC,  /* */ KC_TRNS,  KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
+    {KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_TRNS,    /* */  KC_SPC, KC_SPC, /* */ KC_TRNS,  KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
   },
 
   // TODO: On lower, try and find keys/functions/macros to map to keys labeled Trns
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_SLEP, KC_TRNS,  KC_TRNS, KC_MNXT,  KC_VOLU,  KC_FN15,  /* */  KC_TAB,  KC_DELETE, /* */ KC_TRNS, KC_BTN2,  KC_MS_U, KC_BTN3, KC_ACL2, KC_TRNS},
     {RESET,   KC_TRNS,  KC_TRNS, KC_MPLY,  KC_MUTE,  KC_FN14,  /* */  KC_ESC,  KC_ENTER,  /* */ KC_TRNS, KC_MS_L,  KC_BTN1, KC_MS_R, KC_ACL1, KC_TRNS},
     {KC_TRNS, KC_TRNS,  KC_TRNS, KC_MPRV,  KC_VOLD,  KC_FN13,  /* */  KC_CAPS, KC_RGUI,   /* */ KC_TRNS, KC_BTN5,  KC_MS_D, KC_BTN4, KC_ACL0, KC_TRNS},
-    {KC_TRNS, KC_TRNS,  KC_LCTL, KC_LALT,  KC_LGUI,  KC_TRNS,  /* */        KC_SPC,       /* */ KC_TRNS, KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
+    {KC_TRNS, KC_TRNS,  KC_LCTL, KC_LALT,  KC_LGUI,  KC_TRNS,  /* */  KC_SPC,  KC_SPC,    /* */ KC_TRNS, KC_RALT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT}
   }
 };
 
